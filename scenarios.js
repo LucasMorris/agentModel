a596fa7f9429fd8bf4f26709baa70a648423a99a
LucasMorris <lucasmorris@me.com>
2022-08-18 12:59:14 +0100

    updated scenario interactions values

--- scenarios.js
+++ scenarios.js
@@ -1,9 +1,11 @@
+// Default scenarios to speed up setting up environment
+
 function scenarioOne() {
-    environment.set("population", 200)
+    environment.set("population", 100)
     environment.set("subsidy", 0)
     environment.set("tax", 15)
-    environment.set("subsidyFixed", 1)  // not fixed
-    environment.set("taxFixed", 1)  //not fixed
+    environment.set("subsidyFixed", 1)          // not fixed
+    environment.set("taxFixed", 1)              //not fixed
     environment.set("subsidyIncrement", 10)
     environment.set("taxIncrement", 20)
     environment.set("bikeDistMax", 20)
@@ -11,25 +13,25 @@ function scenarioOne() {
 }
 
 function scenarioTwo() {
-    environment.set("population", 250)
-    environment.set("subsidy", 0)
-    environment.set("tax", 15)
-    environment.set("subsidyFixed", 0)  // not fixed
-    environment.set("taxFixed", 0)  //not fixed
-    environment.set("subsidyIncrement", 10)
-    environment.set("taxIncrement", 20)
-    environment.set("bikeDistMax", 20)
+    environment.set("population", 200)
+    environment.set("subsidy", 5)
+    environment.set("tax", 18)
+    environment.set("subsidyFixed", 1)          // not fixed
+    environment.set("taxFixed", 0)              // fixed
+    environment.set("subsidyIncrement", 12)
+    environment.set("taxIncrement", 0)
+    environment.set("bikeDistMax", 30)
     environment.set("bikeDistMin", 2);
 }
 
 function scenarioThree() {
     environment.set("population", 300)
     environment.set("subsidy", 0)
-    environment.set("tax", 15)
-    environment.set("subsidyFixed", 0)  // not fixed
-    environment.set("taxFixed", 0)  //not fixed
-    environment.set("subsidyIncrement", 10)
-    environment.set("taxIncrement", 20)
-    environment.set("bikeDistMax", 20)
+    environment.set("tax", 25)
+    environment.set("subsidyFixed", 1)          // not fixed
+    environment.set("taxFixed", 1)              //not fixed
+    environment.set("subsidyIncrement", 5)
+    environment.set("taxIncrement", 10)
+    environment.set("bikeDistMax", 30)
     environment.set("bikeDistMin", 2);
 }
